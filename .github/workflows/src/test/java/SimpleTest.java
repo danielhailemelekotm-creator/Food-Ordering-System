6905047ab308a5a45211f0f2d4ba763dcaea0363
danielhailemelekotm-creator <danielhailemelekotm@gmail.com>
2026-05-29 18:11:28 +0300

    Add SimpleTest class with a basic math test

---
+++ .github/workflows/src/test/java/SimpleTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void testMath() {
+        assertEquals(2, 1 + 1);
+    }
+}
